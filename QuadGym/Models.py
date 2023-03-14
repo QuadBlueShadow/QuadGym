09d26f33ccc201c3d8422b76de158131062d73b0
QuadBlueShadow <99780232+QuadBlueShadow@users.noreply.github.com>
2023-03-13 20:03:19 -0500

    Update Models.py

--- QuadGym/Models.py
+++ QuadGym/Models.py
@@ -19,19 +19,3 @@ class BaseModelR():
         actions = np.append(actions, [rand.randint(0, 1)], axis=0)
 
         return actions
-
-class RandomAgentKBM(BaseModelR):
-    def __init__(self, obs):
-        self.n_inputs = 5
-
-    def get_actions(self):
-        #Random actions using KBM action parser
-        actions = np.array([])
-        actions = np.append(actions, [rand.randint(-1, 1)], axis=0)
-        actions = np.append(actions, [rand.randint(-1, 1)], axis=0)
-        actions = np.append(actions, [rand.randint(-1, 1)], axis=0)
-
-        actions = np.append(actions, [rand.randint(0, 1)], axis=0)
-        actions = np.append(actions, [rand.randint(0, 1)], axis=0)
-
-        return actions
